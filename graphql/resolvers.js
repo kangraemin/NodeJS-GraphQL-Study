f33b522c63c185379790273a335814e37050758d
kangraemin <remin1994@gmail.com>
2020-10-01 14:41:14 +0900

    Add rams type for data

--- graphql/resolvers.js
+++ graphql/resolvers.js
@@ -1,8 +1,15 @@
 // Query를 해결 하는 곳
 // Resolver를 DB에 연결하는 등 다양한 작업을 서술 해줄 수 있다. 
+
+const rams = {
+    name: "rams",
+    age: 28,
+    gender: "male"
+}
+
 const resolvers = {
     Query: {
-        name: () => "nicolas"
+        person: () => rams
     }
 };
 
